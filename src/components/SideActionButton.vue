2a501b62ec2cafe7e72884eb94282b0d4643f94a
NathanViaud <79544144+NathanViaud@users.noreply.github.com>
2022-12-20 10:32:09 +0100

    Issue #41: Side Bar (WIP)

---
+++ src/components/SideActionButton.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{
+    icon: string;
+    //TODO: Find a way to manage different actions within the same component
+    //action: string;
+}>();
+
+const showMenu = ref(false);
+
+</script>
+
+<template>
+    <button class="btn" :class="{ active : showMenu }" @click="showMenu = !showMenu">
+        <i :class="icon" />
+    </button>
+</template>
+
+<style scoped lang="scss">
+button {
+    background-color: var(--button-blue);
+    width: 60px;
+    height: 60px;
+    margin-bottom: .7rem;
+    border-radius: 12px;
+    border: 1px solid var(--border);
+    i {
+        margin: auto;
+        font-size: 1.5rem;
+        font-weight: 500;
+    }
+    &:active {
+        background-color: var(--content);
+        box-shadow: 0 1px 8px 0 var(--shadow);
+    }
+
+    &.active {
+        background-color: var(--content);
+        box-shadow: 0 1px 8px 0 var(--shadow);
+    }
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import SideActionButton from './SideActionButton.vue';
+
+const actions = [
+    {
+        icon: 'icon-texte'
+    },
+    {
+        icon: 'icon-video'
+    },
+    {
+        icon: 'icon-audio'
+    },
+    {
+        icon: 'icon-question'
+    },
+    {
+        icon: 'icon-condition'
+    },
+    {
+        icon: 'icon-javascript'
+    },
+];
+</script>
+
+<template>
+    <div class="side-bar">
+        <img src="../assets/img/epoc.svg" alt="logo ePoc">
+        <div class="side-bar-actions">
+            <SideActionButton
+                v-for="(action) in actions"
+                :key="action.icon"
+                :icon="action.icon"
+            />
+            <hr class="separator">
+            <SideActionButton
+                icon="icon-modele"
+            />
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.side-bar {
+    background-color: var(--content);
+    border-right: 1px solid var(--border);
+    img {
+        width: 80%;
+        margin-top: 2rem;
+        margin-left: 10%;
+    }
+    &-actions {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 2rem;
+    }
+}
+</style>
