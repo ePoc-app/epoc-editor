6cb684fcd0ad6bd0b7dea71c60e45bf171308efe
NathanViaud <79544144+NathanViaud@users.noreply.github.com>
2022-12-20 14:21:57 +0100

    Issue #31 : content button (WIP)

---
+++ src/components/ContentButton.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    icon: string;
+    classList: object;
+    isActive?: boolean;
+    isDraggable: boolean;
+}>();
+
+const emit = defineEmits<{
+    (e: 'click'): void;
+}>();
+
+function click() {
+    emit('click');
+}
+</script>
+
+<template>
+    <button
+        class="btn btn-content"
+        :draggable="isDraggable"
+        :class="[classList, { active: props.isActive }]"
+        @click="click"
+    >
+        <i :class="icon" />
+    </button>
+</template>
+
+<style>
+
+</style>
