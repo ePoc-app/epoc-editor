e7d023606fc5f7c2f94c0d1e7e8bbd5d430e872c
NathanViaud <79544144+NathanViaud@users.noreply.github.com>
2022-12-20 10:32:32 +0100

    Issue #40 : Top Bar (WIP)

---
+++ src/components/TopActionButton.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+defineProps<{
+    icon: string;
+    text?: string;
+    textBefore?: string;
+     //TODO: Find a way to manage different actions within the same component
+    //action: string;
+}>();
+
+</script>
+
+<template>
+    <button class="btn btn-top-bar" :class="!text ? 'btn-squared' : ''">
+        <span v-if="textBefore" class="text-top-bar zoom-span">{{ textBefore }}</span>
+        <i :class="icon" />
+        <span v-if="text" class="text-top-bar">{{ text }}</span>
+    </button>
+</template>
+
+<style scoped lang="scss">
+.btn-top-bar {
+    padding: .7rem 1rem;
+    margin-right: 1rem;
+    background-color: var(--button-blue);
+    i {
+        margin: auto;
+        font-size: 1rem;
+    }
+    .text-top-bar {
+        margin-left: .5rem;
+    }
+    .icon-chevron {
+        font-size: .5rem;
+    }
+    .zoom-span {
+        margin-right: .5rem;
+        margin-left: 0;
+    }
+}
+.btn-squared {
+    padding: .7rem;
+}
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import TopActionButton from './TopActionButton.vue';
+</script>
+
+<template>
+    <div class="top-bar">
+        <div class="top-bar-content">
+            <div class="top-bar-title">
+                <h3>L'intelligence artificielle pour tous</h3>
+                <small>Dernière sauvegarde: Il y a 3 minutes</small>
+            </div>
+            <div class="top-bar-actions">
+                <TopActionButton icon="icon-chevron" text-before="100%" />
+                <hr class="vertical-separator">
+                <TopActionButton icon="icon-arriere" />
+                <TopActionButton icon="icon-avant" />
+                <hr class="vertical-separator">
+                <TopActionButton icon="icon-play" text="Aperçu" />
+                <TopActionButton icon="icon-export" text="Exporter archive" />
+                <TopActionButton icon="icon-save" text="Sauvegarder" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.top-bar {
+    background-color: var(--content);
+    border-bottom: 1px solid var(--border);
+    padding-left: 1.7rem;
+
+    &-content {
+        display: flex;
+        height: 100%;
+    }
+    &-title {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        justify-content: center;
+       * {
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        display: block;
+       }
+       min-width: 0;
+    }
+    &-actions {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-left: auto;
+        min-width: fit-content;
+    }
+    h3 {
+        margin: 0 0 .2rem 0;
+
+    }
+    small {
+        color: var(--text-secondary);
+    }
+}
+</style>
