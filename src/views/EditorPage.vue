6d5034ce1cdd22f00ea02b9db53bcf3ed2a321a8
NathanViaud <79544144+NathanViaud@users.noreply.github.com>
2022-12-21 17:08:55 +0100

    Issue #32 : main page(WIP)

---
+++ src/views/EditorPage.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import SideBar from '../components/sideBar/SideBar.vue';
+import TopBar from '../components/topBar/TopBar.vue';
+import ePocFlow from '../components/ePocFlow/ePocFlow.vue';
+import { useEditorStore } from '../shared/stores';
+
+const editorStore = useEditorStore();
+
+function dismissModals() {
+    editorStore.dismissModals();
+}
+
+</script>
+
+<template>
+    <div class="editor-container" @click="dismissModals">
+        <SideBar class="side-bar" />
+        <TopBar class="top-bar" />
+        <ePocFlow class="editor-content" />
+    </div>
+</template>
+
+<style scoped lang="scss">
+.editor-container {
+    display: grid;
+    height: 100%;
+    grid-template-columns: 100px auto;
+    grid-template-rows: 80px auto;
+
+    .side-bar {
+        grid-column: 1;
+        grid-row: 1 / 3;
+    }
+
+    .top-bar {
+        grid-column: 2;
+        grid-row: 1;
+    }
+
+    .editor-content {
+        margin: 0;
+        grid-column: 2;
+        grid-column: 2;
+        margin: auto;
+    }
+}
+</style>
