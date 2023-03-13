d4503ae3e023830bcf2a0ad0ab538fd28c771e67
VIAUD Nathan <nathan.viaud@inria.fr>
2023-03-13 16:05:25 +0000

    Issue #142: refactor page element data

--- src/features/forms/components/GenericField.vue
+++ src/features/forms/components/GenericField.vue
@@ -19,39 +19,34 @@ const editorStore = useEditorStore();
 const node = editorStore.openedParentId ? findNode(editorStore.openedParentId) : findNode(editorStore.openedNodeId);
 
 function onInput(value, id) {
-
-    if(editorStore.openedParentId && node.data.type === 'question') {
-        const contentId = node.data.elements.find(element => element.id === editorStore.openedNodeId).contentId;
-
-        if(!node.data.formValues[contentId]) node.data.formValues[contentId] = {};
-
-        node.data.formValues[contentId][id] = value;
+    if(editorStore.openedParentId) {
+        const element = node.data.elements.find(element => element.id === editorStore.openedNodeId);
+        element.formValues[id] = value;        
     } else {
         node.data.formValues[id] = value;
     }
+
     projectService.writeProjectData();
 }
 
 function onRepeatInput(value, id) {
-
-    let contentId = null;
-    if(editorStore.openedParentId && node.data.type === 'question') {
-        contentId = node.data.elements.find(element => element.id === editorStore.openedNodeId).contentId;
+    let element = null;
+    if(editorStore.openedParentId) {
+        element = node.data.elements.find(element => element.id === editorStore.openedNodeId);
     }
 
     if(value.type === 'add') {
-        if(contentId) {
-            if(!node.data.formValues[contentId]) node.data.formValues[contentId] = {};
-            if(!node.data.formValues[contentId][id]) node.data.formValues[contentId][id] = [];
+        if(element) {
+            if(!element.formValues[id]) element.formValues[id] = [];
 
-            node.data.formValues[contentId][id].push('');
+            element.formValues[id].push('');
         } else {
             if(!node.data.formValues[id]) node.data.formValues[id] = [];
 
             node.data.formValues[id].push('');
         }
     } else if(value.type === 'remove') {
-        contentId ? node.data.formValues[contentId][id].splice(value.index, 1) : node.data.formValues[id].splice(value.index, 1);
+        element ? element.formValues[id].splice(value.index, 1) : node.data.formValues[id].splice(value.index, 1);
 
         if (node.data.elements && node.data.elements[value.index] && !editorStore.openedParentId) {
             const nodeToDelete = node.data.elements[value.index];
@@ -61,9 +56,9 @@ function onRepeatInput(value, id) {
         if (node.data.elements && !editorStore.openedParentId) {
             editorStore.changeElementOrder(value.oldIndex, value.newIndex, node.id);
         } else {
-            if(contentId) {
-                const item = node.data.formValues[contentId][id].splice(value.oldIndex, 1);
-                node.data.formValues[contentId][id].splice(value.newIndex, 0, item[0]);
+            if(element) {
+                const item = element.formValues[id].splice(value.oldIndex, 1);
+                element.formValues[id].splice(value.newIndex, 0, item[0]);
             } else {
                 const item = node.data.formValues[id].splice(value.oldIndex, 1);
                 node.data.formValues[id].splice(value.newIndex, 0, item[0]);
@@ -71,13 +66,13 @@ function onRepeatInput(value, id) {
         }
     } else if(value.type === 'change') {
         if(value.id === '') {
-            contentId ? node.data.formValues[contentId][id][value.index] : node.data.formValues[id][value.index] = value.value;
+            element ? element.formValues[id][value.index] = value.value : node.data.formValues[id][value.index] = value.value;
         } else {
-            if(contentId) {
-                if(!node.data.formValues[contentId][id]) node.data.formValues[contentId][id] = {};
-                if(!node.data.formValues[contentId][id][value.index]) node.data.formValues[contentId][id][value.index] = {};
-
-                node.data.formValues[contentId][id][value.index][value.id] = value.value;
+            if(element) {
+                if(!element.formValues[id]) element.formValues[id] = {};
+                if(!element.formValues[id][value.index]) element.formValues[id][value.index] = {};
+                
+                element.formValues[id][value.index][value.id] = value.value; 
             } else {
                 if(!node.data.formValues[id][value.index]) node.data.formValues[id][value.index] = {};
     
@@ -89,12 +84,12 @@ function onRepeatInput(value, id) {
 }
 
 function getInputValue(input) {
-    if(editorStore.openedParentId && node.data.type === 'question') {
-        const contentId = node.data.elements.find(element => element.id === editorStore.openedNodeId).contentId;
-        if(!node.data.formValues[contentId]) {
-            node.data.formValues[contentId] = {};
-        }
-        return node.data.formValues[contentId][input.id] ? node.data.formValues[contentId][input.id] : input.value;
+    if(editorStore.openedParentId) {
+        const element = node.data.elements.find(element => element.id === editorStore.openedNodeId);
+
+        if(!element.formValues) element.formValues = {};
+
+        return element.formValues[input.id] ? element.formValues[input.id] : input.value;
     } else {
         return node.data.formValues[input.id] ? node.data.formValues[input.id] : input.value;
     }
