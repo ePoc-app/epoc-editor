a7897272a88a709aa27790cfa2ad40006dcc215e
VIAUD Nathan <nathan.viaud@inria.fr>
2023-09-12 10:20:08 +0200

    Issue #234: fix badge icons in prod

--- src/shared/data/badge.data.ts
+++ src/shared/data/badge.data.ts
@@ -1,42 +1,5 @@
-const iconsPath = '/img/badge/icon';
-
-// export const defaultBadges: Badge[] = [
-//     {
-//         id: '1',
-//         title: 'Quizz master',
-//         description: 'Finish all the quizz',
-//         icon: `${iconsPath}/question.svg`,
-//         rule: {},
-//     },
-//     {
-//         id: '2',
-//         title: 'Audio master',
-//         description: 'Listen to all the audio',
-//         icon: `${iconsPath}/audio.svg`,
-//         rule: {},
-//     },
-//     {
-//         id: '3',
-//         title: 'Video master',
-//         description: 'Watch all the video',
-//         icon: `${iconsPath}/video.svg`,
-//         rule: {},
-//     },
-//     {
-//         id: '4',
-//         title: 'Find your path',
-//         description: 'Finish the path',
-//         icon: `${iconsPath}/condition.svg`,
-//         rule: {},
-//     },
-//     {
-//         id: '5',
-//         title: 'Winner',
-//         description: 'Finish the ePoc',
-//         icon: `${iconsPath}/cup.svg`,
-//         rule: {},
-//     }
-// ];
+import env from '@/src/shared/utils/env';
+const iconsPath = env.isDev ? '/img/badge/icon' : 'img/badge/icon';
 
 export const defaultBadgeIcons = [
     `${iconsPath}/audio.svg`,
